253a541eca371d78dd9dbaf65bacc3490a0a0bf2
LiLPandemio <markhaxx@gmail.com>
2023-06-27 05:25:11 +0200

    FIX Playlists not loading

--- main.py
+++ main.py
@@ -24,15 +24,29 @@ for playlist in playlists['items']:
     playlist_name = playlist['name'].replace("/", "_")
     playlist_id = playlist['id']
     playlist_tracks = sp.playlist_tracks(playlist_id, fields='items(track(name,artists,name,id,external_urls))')
-    playlist_name = playlist_name.replace('<', 'x').replace('>', 'x')  # Reemplazar < y > en el nombre de la carpeta
+    playlist_name = playlist_name.replace('<3', '❤️').replace('<', 'x').replace('>', 'x')  # Reemplazar < y > en el nombre de la carpeta
     song_folder = os.path.normpath(os.path.join(output_dir, playlist_name)).replace(":", "")
     output_path = os.path.join(output_dir, playlist_name)
     linx = "cd '" + song_folder + "'\n"
     os.makedirs(output_path, exist_ok=True)
+    #CREAR LA CARPETA DE LA PLAYLIST
+    # Crear la carpeta si no existe de la playlist descargando
+    os.makedirs(song_folder, exist_ok=True)
+
+    archivo_m3u = os.path.join(song_folder, playlist_name + ".m3u")
+    if os.path.exists(archivo_m3u):
+        os.remove(archivo_m3u)
+    with open(archivo_m3u, 'w') as f:
+        pass
     for track in playlist_tracks['items']:
         track_title = track['track']['name']
         track_artist = track['track']['artists'][0]['name']
         track_id = track['track']['id']
+        with open(archivo_m3u, "a", encoding="utf-8") as playlist_file:
+            track_path = os.path.join(output_dir, playlist_name, track_title + " - " + track_artist + ".mp3")
+            playlist_file.write("#EXTINF:-1," + track_artist + " - " + track_title + "\n")
+            playlist_file.write("./" + track_artist + " - " + track_title + ".mp3" + "\n")
+
 
         # Crear la carpeta si no existe de la playlist descargando
         lin1 = "spotdl '" + track_title + " - " + track_artist + "'\n"
